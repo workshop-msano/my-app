ac84aaca88def878bcad56535842abee3daab2ed
miku <svartkatt39@gmail.com>
2022-09-18 18:02:03 +0200

    implement server with express-graphql

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+const axios = require("axios");
+require('dotenv').config();
+
+const schema = buildSchema(`
+
+type Movie{
+    id: Int
+    title: String
+    overview:String
+    poster_path:String
+}
+
+type Query {
+    getPopularMovies:[Movie]
+}
+`);
+
+const root = {
+  getPopularMovies: async () => {
+    const movies = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.APIKEY}`
+    );
+    // console.log("movies", movies.data.results);
+    return movies.data.results;
+  },
+};
+
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => console.log("Server on port 4000"));
